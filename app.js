2ba305172556f94cb2b9e14e01319a5ac12bb75c
Zahraa Alaiwi <zozoa.jalil@gmail.com>
2026-06-21 16:53:18 +0300

    Add game object and exercises for Pokémon management

--- app.js
+++ app.js
@@ -152,3 +152,177 @@ const pokemon = [
   { number: 150, name: 'Mewtwo', type: 'psychic', hp: 106, starter: false },
   { number: 151, name: 'Mew', type: 'psychic', hp: 100, starter: false },
 ];
+
+const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: false, difficulty: 3 },
+    { location: "Celadon City", completed: false, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: false, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: false, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+}
+
+// Exercise 1: Explore the data
+console.log('Exercise 1:');
+console.log(pokemon);
+
+// Exercise 2: Explore the data
+console.log('Exercise 2:');
+console.log(game)
+
+/**
+ * Exercise 3:
+ * 1. Add a new property to the `game` object. Let's call it "difficulty".
+ * 2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+ */
+game.difficulty = 'Hard';
+
+console.log('Exercise 3:' + game.difficulty);
+/**
+ * Exercise 4
+ * 1. Because we console.log(pokemon) before we know which pokemons are the starter pokemon
+ * 2. Remember, a starter Pokémon's `starter` property is true.
+ * 3. Access this pokemon that you want as your starter pokemon in the pokemon array and add them to the game.party array
+ */
+
+game.party.push(pokemon[3]);
+
+console.log('Exercise 4:');
+console.log(game.party);
+
+/**
+ * Exercise 5
+ * 1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+ * 2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+ * 3. HINT: You can use push or unshift 3 times or you can use splice to add 3 at the same time
+ */
+
+game.party.splice(game.party.length,0,pokemon[7],pokemon[24],pokemon[33]);
+
+console.log('Exercise 5:');
+console.log(game.party);
+
+
+/**
+ * Exercise 6
+ * 1. Set the `completed` property to true for gyms with a difficulty below 3.
+ * 2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+ */
+
+game.gyms.forEach(gym => {
+  if(gym.difficulty < 3)
+    gym.completed = true;
+});
+
+console.log('Exercise 6:');
+console.log(game.gyms);
+
+
+/**
+ * Exercise 7
+ *  1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+ *  2. How would you replace the current starter Pokémon in your party with its evolved form?
+ * 
+ *  Hint: 
+ *    - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+ *    - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+ *    - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+ *    - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+ *
+ *  More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+ */
+
+game.party.splice(0, 1, pokemon[1]);
+
+console.log('Exercise 7:')
+console.log(game.party);
+
+
+/**
+ * Exercise 8
+ * 1. Print the name of each Pokémon in your party.
+ * 2. Consider using a loop or an array method to access each Pokémon's name.
+ * 3. HINT: You can use a normal for loop, for of loop or forEach loop
+ */
+
+console.log('Exercise 8:');
+game.party.forEach(p => {
+  console.log(p.name);
+});
+
+
+/**
+ * Exercise 9
+ * 1. Can you print out all the starter Pokémon from the `pokemon` array?
+ * 2. Think about how you can identify a starter Pokémon and then log their names.
+ * 3. HINT: You can use a normal for loop, for of loop or forEach loop (with an if statement)
+ */
+
+console.log('Exercise 9:')
+pokemon.forEach(p => {
+  if(p.starter)
+    console.log(p.name)
+})
+
+/**
+ * Exercise 10
+ * Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+ *  - Accept an object as a parameter called `pokemonObj`
+ *  - Add the `pokemonObj` to the `game.party` array.
+ *  - not return anything
+ *
+ * After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+ */
+game.catchPokemon = (pokemonObj) => {game.party.push(pokemonObj)};
+game.catchPokemon(pokemon[44]);
+
+console.log('Exercise 10:');
+console.log(game.party);
+
+
+/**
+ * Exercise 11
+ * 1. Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify it so that it also decreases the number of pokeballs in your inventory each time you catch a Pokémon.
+ * 2. How will you find and update the quantity of pokeballs in the `game.items` array?
+
+ * Tips:
+ * For this exercise, it's okay to have a negative number of pokeballs.
+ * After updating the method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+ * Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+*/
+console.log('Exercise 11:');
+
+function catchPokemon(pokemonObj) {
+  game.party.push(pokemonObj);
+  --game.items[1].quantity;
+}
+
+catchPokemon(pokemon[67]);
+console.log(game.items[1]);
+
+
+/**
+ * Exercise 12
+ * 1. Similar to Exercise 6, now complete gyms with a difficulty below 6. How will you approach this?
+ *    (change the value of `complete` in the qualifying objects from false to true).
+ */
+game.gyms.forEach(gym =>{
+  if(gym.difficulty < 6)
+    gym.completed = true;
+});
+
+console.log('Exercise 12:');
+console.log(game.gyms);
+
